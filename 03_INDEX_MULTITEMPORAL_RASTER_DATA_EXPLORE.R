903a2f1b6d059c361f77fbf0b73974d38daed7ba
Iskar Waluyo <iskarwaluyo@gmail.com>
2024-06-23 18:18:28 -0700

    Update

--- 03_INDEX_MULTITEMPORAL_RASTER_DATA_EXPLORE.R
+++ 03_INDEX_MULTITEMPORAL_RASTER_DATA_EXPLORE.R
@@ -11,129 +11,90 @@ library(data.table)
 # load("~/sigdata/archivos2/sigdata/PROYECTOS/MULTITEMPSTACKER/DATA/RDATA/INDEX_CALCULATION.RData")
 # load("~/sigdata/archivos2/sigdata/PROYECTOS/MULTITEMPSTACKER/DATA/RDATA/RASTER_STACK.RData")
 
-setwd("~/sigdata/archivos2/sigdata/PROYECTOS/MULTITEMPSTACKER/R/")
+setwd("~/archivos2/sigdata/PROYECTOS/MULTITEMPSTACKER/R/")
 
-
-# RASTER TO DATA FRAME WITH MONTH AND YEAR FACTORS ('DUMMY VARIABLES')
-index_time_melt <- function(index){
+index_time_melt <- function(index, rainy_months){
   
-  comando <<- paste0(index, "_df <<- as.data.frame(",index,")")
-  eval(parse(text = comando))
-  
-  comando <<- paste0("names(",index,"_df) <<- time_line")
-  eval(parse(text = comando))
-  
-  comando <<- paste0(index, "_df_melt <<- melt(",index,"_df, level = 2)")
-  eval(parse(text = comando))
-  
-  #comando <<- paste0(index, "_df_melt <<- na.omit(",index,"_df_melt)")
-  #eval(parse(text = comando))
-  
-  comando <<- paste0(index, "_df_melt$month <<- format(as.POSIXct(",index,"_df_melt$variable),'%m')")
-  eval(parse(text = comando))
-  
-  comando <<- paste0(index, "_df_melt$year <<- format(as.POSIXct(",index,"_df_melt$variable),'%Y')")
+  comando <- paste0('index_data <<- eval(',index,')')
   eval(parse(text = comando))
 
-}
-
-index_season_melt <- function(index, low, high){
+  index_melt_df <<- list()
+  index_melt_df <<- as.data.frame(index_data)
+  names(index_melt_df) <<- time_line
+  index_melt_df <<- melt(as.data.table(index_melt_df), level = 2)
+  index_melt_df <<- na.omit(index_melt_df)
+  index_melt_df$month <<- format(as.POSIXct(index_melt_df$variable),'%m')
+  index_melt_df$year <<- format(as.POSIXct(index_melt_df$variable),'%Y')
+  index_melt_df$season <<- ifelse(grepl(paste(rainy_months, collapse="|"), 
+                                        index_melt_df$month) == TRUE, 'High precipitation', 'Low precipitation')
   
-  comando <<- paste0(index, "_df_melt$season <<- ifelse("
-                     ,index,"_df_melt[,3] == '01' | "
-                     ,index,"_df_melt[,3] == '02' | "
-                     ,index,"_df_melt[,3] == '03' | "
-                     ,index,"_df_melt[,3] == '04' | "
-                     ,index,"_df_melt[,3] == '11' | "
-                     ,index,"_df_melt[,3] == '12', ", low,",", high,")")
-  eval(parse(text = comando))
+  index_melt_df <<- as.data.frame(index_melt_df)
   
-}
-
-index_signature_grids <- function(index, include_season){
+  if(index == 'ndvi'){index_name = 'Normalized Difference Vegetation Index'}
+  if(index == 'avi'){index_name = 'Advanced Vegetation Index'}
+  if(index == 'savi'){index_name = 'Soil Adjusted Vegetation Index'}
+  if(index == 'bsi'){index_name = 'Barren Soil Index'}
+  if(index == 'ndmi'){index_name = 'Normalized Difference Moisture Index'}
+  if(index == 'ndwi'){index_name = 'Normalized Difference Water Index'}
+  if(index == 'ui'){index_name = 'Urbanization Level Index'}
+  if(index == 'mbi'){index_name = 'Modified Built-Up Index'}
+  if(index == 'baei'){index_name = 'Built up Area Index'}
   
-  # PLOTTING INDEX RESULTS
-  comando <<- paste0(index,"_signature_plots_month <<- ggplot(data=",index,"_df_melt, aes(x = value, fill=year)) + 
-  geom_density(adjust=1.5, alpha=.4) + 
-  facet_wrap(vars(month), ncol = 3) + 
-  theme(legend.position='right') +
-  scale_fill_brewer(palette='Dark2')")
-  eval(parse(text = comando))
+  year_melt <<-ddply(index_melt_df, .(year), summarize, mean=mean(value))
+  year_season_melt <<-ddply(index_melt_df, .(season, year), summarize, mean=mean(value))
+  year_month_melt <<-ddply(index_melt_df, .(month, year), summarize, mean=mean(value))
+  season_melt <<-ddply(index_melt_df, .(year, season), summarize, mean=mean(value))
   
-  comando <<- paste0(index,"_signature_plots_year <<- ggplot(data=",index,"_df_melt, aes(x = value, fill=month)) + 
-  geom_density(adjust=1.5, alpha=.4) + 
-  facet_wrap(vars(year), ncol = 3) + 
-  theme(legend.position='right') +
-  scale_fill_brewer(palette='Paired')")
-  eval(parse(text = comando))
+  signature_plots_month <<- ggplot(data=index_melt_df, aes(x = value, fill=year)) + 
+    geom_density(adjust=1.5, alpha=.4) +
+    labs(title= paste0("Density Plot ", index_name), subtitle = "Month compare by year",
+         y="Density",
+         x="Index Score") +
+    facet_wrap(vars(month), ncol = 3) + 
+    theme(legend.position='right') +
+    scale_fill_brewer(palette='Dark2')
   
-if(include_season == TRUE){
-  comando <<- paste0(index,"_signature_plots_season <<- ggplot(data=",index,"_df_melt, aes(x = value, fill=year)) + 
-  geom_density(adjust=1.5, alpha=.4) + 
-  facet_wrap(vars(season), ncol = 3) + 
-  theme(legend.position='right') +
-  scale_fill_brewer(palette='Accent')")
-  eval(parse(text = comando))
-}
-
-}
-
-# index_signature_grids('baei', FALSE)
-
-
-index_month_compare <- function(index_melt_select, y, title){
-  x <- index_melt_select
-  index_select <<- subset(x, x$month == y)
-  ggplot(data=index_select, aes(x=value, fill=year)) + 
-  geom_density(adjust=1.5, alpha=.4) + 
-    labs(title=title,
+  signature_plots_year <<- ggplot(data=index_melt_df, aes(x = value, fill=month)) + 
+    geom_density(adjust=1.5, alpha=.4) + 
+    labs(title= paste0("Density Plot ", index_name), subtitle = "Year compare by month",
+         y="Density",
+         x="Index Score") +
+    facet_wrap(vars(year), ncol = 3) + 
+    theme(legend.position='right') +
+    scale_fill_brewer(palette='Paired')
+  
+  signature_plots_month_year <<- ggplot(data=index_melt_df, aes(x=value, fill=year)) + 
+    geom_density(adjust=1.5, alpha=.4) + 
+    labs(title= paste0(index_name, " Density Plot"), subtitle = "Season compare by year",
          y="Density",
          x="Index Score") +
     theme(legend.position = "none")  +
     scale_fill_brewer(palette = "Set3") +
-   facet_wrap(vars(year)) + 
-  theme(legend.position="bottom")
-}
-
-
-index_month_year <- function(index_melt_select, plot_title){
-  x <- index_melt_select
-  index_select <<- subset(x)
-  ggplot(data=index_select, aes(x=value, fill=year)) + 
-    geom_density(adjust=1.5, alpha=.4) + 
-    labs(title=plot_title, subtitle = "Month compare by year",
-         y="Density",
-         x="Index Score") +
-    theme(legend.position = "none")  +
-    scale_fill_brewer(palette = "Set3") +
-    facet_wrap(vars(month)) + 
-    theme(legend.position="bottom")
-}
-
-# index_month_year(baei_df_melt, "BAEI")
-
-# ONLY WORKS IF SEASON = TRUE AND SEASON PARAMETERS ARE DESIGNED
-
-index_season_year <- function(index_melt_select, plot_title){
-  x <- index_melt_select
-  index_select <<- subset(x)
-  ggplot(data=index_select, aes(x=value, fill=year)) + 
-    geom_density(adjust=1.5, alpha=.4) + 
-    labs(title=plot_title, subtitle = "Season compare by year",
-         y="Density",
-         x="Index Score") +
-    theme(legend.position = "none") +
-    scale_fill_brewer(palette = "Set3") + 
     facet_wrap(vars(season)) + 
     theme(legend.position="bottom")
+  
+  signature_plots_season_year <<- ggplot(data=index_melt_df, aes(x=value, fill=season)) + 
+    geom_density(adjust=1.5, alpha=.4) + 
+    labs(title= paste0("Density plot ", index_name), subtitle = "Year compare by season",
+         y="Density",
+         x="Index Score") +
+    theme(legend.position = "none")  +
+    scale_fill_brewer(palette = "Set3") +
+    facet_wrap(vars(year)) + 
+    theme(legend.position="bottom")
+  
 }
 
-# index_season_year(baei_df_melt, "BAEI")
+rainy_months = c('05', '06', '07', '08', '09', '10')
 
-save(index_time_melt, index_season_melt, index_signature_grids, 
-     index_month_compare, index_month_year, index_season_year, 
-     file = '~/sigdata/archivos2/sigdata/PROYECTOS/MULTITEMPSTACKER/DATA/RDATA/INDEX_MELT.RData')
+index_time_melt('baei', rainy_months)
 
 
-dfs <- ls()[sapply(mget(ls(), .GlobalEnv), is.data.frame)]
-rm(dfs)
+year_melt
+year_season_melt
+year_month_melt
+season_melt
+
+signature_plots_month
+signature_plots_season_year
+signature_plots_month_year
